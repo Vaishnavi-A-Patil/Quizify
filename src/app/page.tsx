9115c33802e0b33729ef59143eaf286b0bcf7477
Vaishnavi Patil <vaishnavipatil.3012@gmail.com>
2025-10-01 17:09:47 +0000

    add Refine Quiz section at the bottom of the Quiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -127,7 +127,7 @@ export default function Home() {
           {!activeSession ? (
             <UploadView onGenerate={handleGenerateQuiz} loading={isGenerating} />
           ) : (
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 flex-1 min-h-0">
+            <div className="flex flex-col gap-6 flex-1 min-h-0">
               <QuizView
                 key={activeSession.id}
                 session={activeSession}
